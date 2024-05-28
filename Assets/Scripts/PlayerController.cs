f91e053df0fc664665c068d6434ed96ab97905d6
nobi-onway <doangiabao.dev@gmail.com>
2024-05-28 12:57:33 +0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private const float SPEED = 20.0f;
+    private const float TURN_SPEED = 45.0f;
     void Update()
     {
-        transform.Translate(Vector3.forward * SPEED * Time.deltaTime);
+        MoveForward();
+        ChangeDirection();
+    }
+
+    private void MoveForward()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * SPEED * verticalInput * Time.deltaTime);
+    }
+
+    private void ChangeDirection()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * TURN_SPEED * horizontalInput * Time.deltaTime);
     }
 }
